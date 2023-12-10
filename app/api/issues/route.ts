04170fe1f8267a1af28f9ce63913577eb8324af9
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-11 02:23:44 +0530

    handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const schema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
 });
 
 export async function POST(request: NextRequest) {
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
   const validattion = schema.safeParse(body);
 
   if (!validattion.success) {
-    return NextResponse.json(validattion.error.errors, { status: 400 });
+    return NextResponse.json(validattion.error.format(), { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: {
