8a508e991e39ad0afe055a0d92de6945f1cae65b
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-10 12:44:19 +0530

    customize radix theme

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -22,14 +22,14 @@ const Navbar = () => {
   return (
     <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14 shadow-md tracking-wide text-[.9rem]">
       <Link href="/">
-        <AiFillBug className="text-violet-900 text-lg" />
+        <AiFillBug className="text-violet-600 text-lg" />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.href}>
             <Link
               className={classnames({
-                "bg-violet-900 p-1.5 px-2 text-white rounded-md":
+                "bg-violet-600 p-1.5 px-2 text-white rounded-md":
                   link.href === currentPath,
                 "text-zinc-950": link.href !== currentPath,
                 "transition-colors": true,
