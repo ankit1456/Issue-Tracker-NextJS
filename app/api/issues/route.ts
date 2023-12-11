f9d22c708071c913001dcaf330b29f4cf6c3ab73
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-11 12:23:16 +0530

    build issue status badge

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,10 +5,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  const validattion = createIssueSchema.safeParse(body);
+  const validation = createIssueSchema.safeParse(body);
 
-  if (!validattion.success) {
-    return NextResponse.json(validattion.error.format(), { status: 400 });
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: {
