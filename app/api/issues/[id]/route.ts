8982dc31d6123a7ee9ffa8432d32f25b12d3bc51
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-14 13:22:27 +0530

    build api for updating issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,35 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { data } from "autoprefixer";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PATCH = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const body = await request.json();
+
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: params.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+};
