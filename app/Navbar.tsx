b470762b7cc3e8cdd60a152381b50d9155a29e34
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-19 19:45:55 +0530

    refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -15,8 +15,64 @@ import {
 } from "@radix-ui/themes";
 
 const Navbar = () => {
-  const currentPath = usePathname();
+  return (
+    <nav className="border-b py-4 px-5 shadow-md tracking-wide text-[.9rem]">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug className="text-violet-600 text-lg" />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const AuthStatus = () => {
   const { data: session, status } = useSession();
+
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            size="2"
+            radius="full"
+            fallback="?"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user?.name}</Text>
+          </DropdownMenu.Label>
+
+          <DropdownMenu.CheckboxItem>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.CheckboxItem>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
 
   const links = [
     {
@@ -28,67 +84,23 @@ const Navbar = () => {
       href: "/issues/list",
     },
   ];
-
   return (
-    <nav className="border-b py-4 px-5 shadow-md tracking-wide text-[.9rem]">
-      <Container>
-        <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link href="/">
-              <AiFillBug className="text-violet-600 text-lg" />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classnames({
-                      "bg-violet-600 p-1.5 px-2 text-white rounded-md":
-                        link.href === currentPath,
-                      "text-zinc-950": link.href !== currentPath,
-                      "transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    size="3"
-                    radius="full"
-                    fallback="?"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user?.name}</Text>
-                  </DropdownMenu.Label>
-
-                  <DropdownMenu.CheckboxItem>
-                    <Link href="/api/auth/signout">Log out</Link>
-                  </DropdownMenu.CheckboxItem>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "bg-violet-600 p-1.5 px-2 !text-white rounded-md":
+                link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
