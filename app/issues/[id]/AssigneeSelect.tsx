1f34b8d5372d9bd1b14c139ad661c8c0aba5af92
Ankit Tripathi <ankittripathi1456@gmail.com>
2023-12-26 22:45:57 +0530

    change user url

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -50,7 +50,10 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 const useUsers = () =>
   useQuery<User[]>({
     queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    queryFn: () =>
+      axios
+        .get("https://trackpulse.vercel.app/api/users")
+        .then((res) => res.data),
     staleTime: 60 * 1000, // 60s
     retry: 3,
   });
